2c977b3452f745e8cca2a04d3bdc66fe0b923fb5
Kelley702 <122461512+Kelley702@users.noreply.github.com>
2023-02-08 22:24:16 -0500

    Update Ex Decay R Code.R

--- Ex Decay R Code.R
+++ Ex Decay R Code.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Create a data frame with time values from 0 to 5
 t <- seq(from = 0, to = 5, by = 0.1)
 
@@ -8,8 +10,47 @@ WhiteAsh <- exp(-0.47 * t)
 # Combine the time and percent mass values into a data frame
 df <- data.frame(t = t, X1 = X1, X2 = X2)
 
+# Plot the percent mass remaining over time
+ggplot(df, aes(x = t, y = Beech)) +
+  geom_line(color = "green") +
+  geom_line(aes(y = WhiteAsh), color = "red") +
+  xlab("Time (t)") +
+  ylab("Percent Mass Remaining (X)") +
+  ggtitle("Exponential Decay of Mass over Time") +
+  scale_y_log10() +
+  scale_x_continuous(limits = c(0, 5), expand = c(0, 0)) +
+  scale_color_manual(values = c("green", "gray")) +
+  theme_classic()
 
-#plotting the graph, where the annotate function adds the equation label and the scale_color_manual adds the legend
+#Making a graph with a legend
+
+ggplot(df, aes(x = t, y = Beech, color = "Beech")) +
+     geom_line() +
+geom_line(aes(y = WhiteAsh, color = "WhiteAsh")) +
+ xlab("Time (t)") +
+ylab("Percent Mass Remaining (X)") +
+ggtitle("Exponential Decay of Mass over Time") +
+ scale_y_log10() +
+scale_x_continuous(limits = c(0, 5), expand = c(0, 0)) +
+ scale_color_manual(name = "Species", values = c("Beech" = "green", "WhiteAsh" = "gray")) +
+guides(color = guide_legend(title.position = "top", title.hjust = 0.5)) +
+ theme_classic()
+
+ #adding the annotate function to the ggplot function
+ggplot(df, aes(x = t, y = Beech)) +
+  geom_line(color = "green") +
+  geom_line(aes(y = WhiteAsh), color = "red") +
+  annotate(geom = "text", x = 2, y = 1, label = expression(paste("X = e^{-0.08t}")), size = 4) +
+  annotate(geom = "text", x = 3, y = 0.21, label = expression(paste("X= e^{-0.47t}")), size = 4) +
+  xlab("Time (t)") +
+  ylab("Percent Mass Remaining (X)") +
+  ggtitle("Exponential Decay of Mass over Time") +
+  scale_y_log10() +
+  scale_x_continuous(limits = c(0, 5), expand = c(0, 0)) +
+  scale_color_manual(values = c("green", "gray")) +
+  theme_classic()
+
+#Making a graph with a legend
 
 ggplot(df, aes(x = t, y = Beech, color = "Beech")) +
   geom_line() +
@@ -17,7 +58,6 @@ ggplot(df, aes(x = t, y = Beech, color = "Beech")) +
   xlab("Time (t)") +
   ylab("Percent Mass Remaining (X)") +
   ggtitle("Exponential Decay of Mass over Time") +
-  #annotate function adds the equation label
   annotate(geom = "text", x = 2, y = 1, label = expression(paste("X = e^{-0.08t}")), size = 4) +
   annotate(geom = "text", x = 3, y = 0.21, label = expression(paste("X= e^{-0.47t}")), size = 4) +
   scale_y_log10() +
